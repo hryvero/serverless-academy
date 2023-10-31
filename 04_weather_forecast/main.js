b38368a2fc9fa5ef75c32abbc2d3d44b9788ed2a
Veronika <146069975+veromashka@users.noreply.github.com>
2023-10-31 16:00:06 +0200

    feat: finish 4 task

--- 04_weather_forecast/main.js
+++ 04_weather_forecast/main.js
@@ -2,73 +2,112 @@ const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 
 token = '6906245106:AAE6FdcrYiEb3l73qKDTwi_1cZUhi489ZVU'
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, {polling: true,none_stop: true});
 let chatId = 608570756;
 
 
 bot.onText(/\/start/, (msg) => {
     bot.sendMessage(msg.chat.id, "Welcome", {
         "reply_markup": {
-            "keyboard": [["Forecast in Lviv"],   ["Forecast in Vinnytsia"]]
+            "keyboard": [["Forecast in Lviv"], ["Forecast in Vinnytsia"]]
         }
     });
 
 });
+
 bot.on('message', (msg) => {
     const lviv = "Forecast in Lviv";
     if (msg.text.indexOf(lviv) === 0) {
-        bot.sendMessage(chatId, "Please set interval",{
+        bot.sendMessage(chatId, "Please set interval", {
             "reply_markup": {
-                "keyboard": [["at intervals of 3 hours"],   ["at intervals of 6 hours"]]
+                "keyboard": [["at intervals of 3 hours"], ["at intervals of 6 hours"]]
             }
         });
 
-        bot.on('message',(answer)=>{
-            const lviv_forecast = {
-                lat: 49.84,
-                lon: 24.03
+        bot.on('message', (answer) => {
+            const lviv_coordinates = {
+                lat:49.84,
+                lon:24.03
             }
             const duration = "at intervals of 3 hours";
             if (answer.text.indexOf(duration) === 0) {
-                console.log("3 intervals")
-                // bot.sendMessage(chatId, "Please set interval");
-                loadForecast(lviv_forecast)
-            }else {
-                console.log("else block")
+                loadForecast(lviv_coordinates)
+                const myInterval=setInterval(()=>{loadForecast(lviv_coordinates)},  3 * 60 * 60 * 1000 )
+            } else {
+                loadForecast(lviv_coordinates)
+                const myInterval=setInterval(()=>{loadForecast(lviv_coordinates)}, 6 * 60 * 60 * 1000)
             }
+
+
         })
+    }
+    const vinnytsia = "Forecast in Vinnytsia";
+    if(msg.text.indexOf(vinnytsia) === 0) {
+
+                bot.sendMessage(chatId, "Please set interval", {
+                    "reply_markup": {
+                        "keyboard": [["at intervals of 3 hours"], ["at intervals of 6 hours"]]
+                    }
+                });
+                bot.on('message', (answer) => {
+                    const vinnytsia_coordinates = {
+                        lat: 49.24,
+                        lon: 28.47
+                    }
+                    const duration = "at intervals of 3 hours";
+                    if (answer.text.indexOf(duration) === 0) {
+                        loadForecast(vinnytsia_coordinates)
+                        setInterval(()=>{loadForecast(vinnytsia_coordinates)}, 3 * 60 * 60 * 1000)
+                    } else {
+                        loadForecast(vinnytsia_coordinates)
+                        const myInterval=setInterval(()=>{loadForecast(vinnytsia_coordinates)}, 6 * 60 * 60 * 1000)
+                    }
+                })
+
+    }
+});
+
+    const loadForecast = async (city) => {
+        const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&appid=0a451ab8ca66d6bab87246de4c00b68e`)
+            .then((response) => {
+                return response.data
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+                return error.message
+
+            })
+
+        const text = {
+            time: new Date,
+            city: result.name,
+            main: result.weather[0].description,
+            humidity: result.main.humidity,
+            temp: (result.main.temp - 273.15).toFixed(2) + " C",
+            wind: result.wind.speed + "km/h"
+        }
+        let resultString = "";
+        for (const key in text) {
+            if (text.hasOwnProperty(key)) {
+                resultString += key + ": " + text[key] + ", \n ";
+            }
+        }
+
+
+// Remove the trailing comma and space
+        resultString = resultString.slice(0, -2);
+        bot.sendMessage(chatId, resultString)
 
     }
 
+bot.onText(/\/stop/, (msg) => {
+    bot.sendMessage(chatId, "I hope you liked it!").then(()=>{
+        clearInterval(myInterval)
+    })
+
+    ;
+
 });
 
-
-const loadForecast=(city)=>{
-    //TODO
-    // axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}.84&lon=${city.lon}&appid=0a451ab8ca66d6bab87246de4c00b68e`)
-    //     .then(function (response) {
-    //         // handle success
-    //         console.log(response);
-    //         return response
-    //     })
-    //     .catch(function (error) {
-    //         // handle error
-    //         console.log(error);
-    //         return error.message
-    //
-    //     })
-    console.log(city.lat)
-}
-
-
-// https://api.openweathermap.org/data/2.5/weather?lat=49.84&lon=24.03&appid=0a451ab8ca66d6bab87246de4c00b68e
-// axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lviv_forecast.lat}.84&lon=${lviv_forecast.lon}&appid=0a451ab8ca66d6bab87246de4c00b68e`)
-//     .then(function (response) {
-//         // handle success
-//         console.log(response);
-//     })
-//     .catch(function (error) {
-//         // handle error
-//         console.log(error);
-//     })
 

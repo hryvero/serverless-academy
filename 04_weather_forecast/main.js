10a59dff4128c09434004347ca144f372286b6c6
Veronika <146069975+veromashka@users.noreply.github.com>
2023-11-01 13:41:10 +0200

    fix: refactor 4 task

--- 04_weather_forecast/main.js
+++ 04_weather_forecast/main.js
@@ -2,12 +2,17 @@ const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 
 token = '6906245106:AAE6FdcrYiEb3l73qKDTwi_1cZUhi489ZVU'
-const bot = new TelegramBot(token, {polling: true,none_stop: true});
+const bot = new TelegramBot(token, {polling: true, none_stop: true});
 let chatId = 608570756;
+let intervalId;
 
+const myInterval = (someFunction, time) => {
+    intervalId = setInterval(() => someFunction, time)
+}
 
-bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, "Welcome", {
+bot.onText(/\/start/, () => {
+    clearInterval(intervalId)
+    bot.sendMessage(chatId, "Welcome", {
         "reply_markup": {
             "keyboard": [["Forecast in Lviv"], ["Forecast in Vinnytsia"]]
         }
@@ -15,99 +20,85 @@ bot.onText(/\/start/, (msg) => {
 
 });
 
+bot.onText(/\/stop/, (msg) => {
+    bot.sendMessage(chatId, "I hope you liked it!", clearInterval(intervalId))
+
+});
+
 bot.on('message', (msg) => {
-    const lviv = "Forecast in Lviv";
-    if (msg.text.indexOf(lviv) === 0) {
-        bot.sendMessage(chatId, "Please set interval", {
-            "reply_markup": {
-                "keyboard": [["at intervals of 3 hours"], ["at intervals of 6 hours"]]
-            }
-        });
+    const messageText = msg.text;
+    if (messageText === "Forecast in Lviv") {
+        const lviv_coordinates = {
+            lat: 49.84,
+            lon: 24.03
+        };
+        sendInterval(msg.chat.id, lviv_coordinates);
+    }
 
-        bot.on('message', (answer) => {
-            const lviv_coordinates = {
-                lat:49.84,
-                lon:24.03
-            }
-            const duration = "at intervals of 3 hours";
-            if (answer.text.indexOf(duration) === 0) {
-                loadForecast(lviv_coordinates)
-                const myInterval=setInterval(()=>{loadForecast(lviv_coordinates)},  3 * 60 * 60 * 1000 )
-            } else {
-                loadForecast(lviv_coordinates)
-                const myInterval=setInterval(()=>{loadForecast(lviv_coordinates)}, 6 * 60 * 60 * 1000)
-            }
+    if (messageText === "Forecast in Vinnytsia") {
+        const vinnytsia_coordinates = {
+            lat: 49.24,
+            lon: 28.47
+        };
+        sendInterval(msg.chat.id, vinnytsia_coordinates);
+    }
+});
 
+function sendInterval(chatId, city) {
+
+    bot.sendMessage(chatId, "Please set interval", {
+        "reply_markup": {
+            "keyboard": [["at intervals of 3 hours"], ["at intervals of 6 hours"], ["/stop"]]
+        }
+    });
+
+    bot.on('message', (answer) => {
+        const duration = answer.text;
+        if (duration === "at intervals of 3 hours") {
+            intervalId = setInterval(() => {
+                loadForecast(city)
+            }, 1000)
+
+        } else if (duration === "at intervals of 6 hours")
+        {
+            intervalId = setInterval(() => {
+                loadForecast(city)
+            }, 6 * 60 * 60 * 1000)
+        }
+    });
+}
+
+const loadForecast = async (city) => {
+    const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&appid=0a451ab8ca66d6bab87246de4c00b68e`)
+        .then((response) => {
+            return response.data
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+            return error.message
 
         })
+
+    const text = {
+        time: new Date,
+        city: result.name,
+        main: result.weather[0].description,
+        humidity: result.main.humidity,
+        temp: (result.main.temp - 273.15).toFixed(2) + " C",
+        wind: result.wind.speed + "km/h"
     }
-    const vinnytsia = "Forecast in Vinnytsia";
-    if(msg.text.indexOf(vinnytsia) === 0) {
-
-                bot.sendMessage(chatId, "Please set interval", {
-                    "reply_markup": {
-                        "keyboard": [["at intervals of 3 hours"], ["at intervals of 6 hours"]]
-                    }
-                });
-                bot.on('message', (answer) => {
-                    const vinnytsia_coordinates = {
-                        lat: 49.24,
-                        lon: 28.47
-                    }
-                    const duration = "at intervals of 3 hours";
-                    if (answer.text.indexOf(duration) === 0) {
-                        loadForecast(vinnytsia_coordinates)
-                        setInterval(()=>{loadForecast(vinnytsia_coordinates)}, 3 * 60 * 60 * 1000)
-                    } else {
-                        loadForecast(vinnytsia_coordinates)
-                        const myInterval=setInterval(()=>{loadForecast(vinnytsia_coordinates)}, 6 * 60 * 60 * 1000)
-                    }
-                })
-
-    }
-});
-
-    const loadForecast = async (city) => {
-        const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&appid=0a451ab8ca66d6bab87246de4c00b68e`)
-            .then((response) => {
-                return response.data
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-                return error.message
-
-            })
-
-        const text = {
-            time: new Date,
-            city: result.name,
-            main: result.weather[0].description,
-            humidity: result.main.humidity,
-            temp: (result.main.temp - 273.15).toFixed(2) + " C",
-            wind: result.wind.speed + "km/h"
+    let resultString = "";
+    for (const key in text) {
+        if (text.hasOwnProperty(key)) {
+            resultString += key + ": " + text[key] + ", \n ";
         }
-        let resultString = "";
-        for (const key in text) {
-            if (text.hasOwnProperty(key)) {
-                resultString += key + ": " + text[key] + ", \n ";
-            }
-        }
-
-
-// Remove the trailing comma and space
-        resultString = resultString.slice(0, -2);
-        bot.sendMessage(chatId, resultString)
-
     }
+    resultString = resultString.slice(0, -2);
+    bot.sendMessage(chatId, resultString)
 
-bot.onText(/\/stop/, (msg) => {
-    bot.sendMessage(chatId, "I hope you liked it!").then(()=>{
-        clearInterval(myInterval)
-    })
+}
 
-    ;
 
-});
 
 
